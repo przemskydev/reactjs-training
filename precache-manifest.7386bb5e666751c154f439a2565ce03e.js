7d9299d796346cc7aa5e028c997ee48d38ca91e4
przemskydev <przemekgithub@o2.pl>
2020-03-26 15:47:41 +0100

    Updates

---
+++ precache-manifest.7386bb5e666751c154f439a2565ce03e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6860fb457bbe7e91edab3a438abcfd84",
+    "url": "/reactjs-training/index.html"
+  },
+  {
+    "revision": "b490f53fd6fc01c0011d",
+    "url": "/reactjs-training/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7d77cdc6e12df407a6f0",
+    "url": "/reactjs-training/static/css/main.b6732865.chunk.css"
+  },
+  {
+    "revision": "b490f53fd6fc01c0011d",
+    "url": "/reactjs-training/static/js/2.8eedd3be.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/reactjs-training/static/js/2.8eedd3be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d77cdc6e12df407a6f0",
+    "url": "/reactjs-training/static/js/main.03483930.chunk.js"
+  },
+  {
+    "revision": "80ca8d987cfd60056d9e",
+    "url": "/reactjs-training/static/js/runtime-main.04c4c258.js"
+  }
+]);
